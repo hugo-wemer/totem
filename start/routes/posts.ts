99251e45f89d8233901fd8cee8b346890aece0ae
Hugo Wemer <73181978+hugo-wemer@users.noreply.github.com>
2021-10-30 13:55:45 -0300

    Update posts.ts

--- start/routes/posts.ts
+++ start/routes/posts.ts
@@ -5,6 +5,6 @@ Route.group(() => {
   Route.post('/', 'PostsController.store').middleware('auth')
 
   Route.get('/:id', 'PostsController.show')
-  Route.put('/:id', 'PostsController.update')//.middleware('auth')
+  Route.put('/:id', 'PostsController.update').middleware('auth')
   Route.delete('/:id', 'PostsController.destroy').middleware('auth')
 }).prefix('/posts')
