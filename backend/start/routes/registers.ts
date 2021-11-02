9adf3ee71d3793561bf870bee148e8fb9004ab4c
Hugo Wemer <73181978+hugo-wemer@users.noreply.github.com>
2021-11-01 23:58:54 -0300

    Delete backend directory

--- backend/start/routes/registers.ts
+++
@@ -1,9 +0,0 @@
-import Route from '@ioc:Adonis/Core/Route'
-
-Route.group(() => {
-  Route.get('/', 'User/Register.index')
-  Route.post('/', 'User/Register.store').middleware('acl:master')
-  Route.get('/:id', 'User/Register.show')
-  Route.put('/:id', 'User/Register.update').middleware('acl:master')
-  Route.delete('/:id', 'User/Register.destroy').middleware('acl:master')
-}).prefix('/registers')
